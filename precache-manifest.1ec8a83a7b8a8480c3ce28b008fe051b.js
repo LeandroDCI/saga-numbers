1b8d045f6ca2f7f2f923c22885f0b337580c40a1
Leandro <leandro.frigerio@devugees.org>
2019-06-13 21:39:14 +0200

    Updates

---
+++ precache-manifest.1ec8a83a7b8a8480c3ce28b008fe051b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195ddb0d68df696ca86ae8e4a55bcc49",
+    "url": "/saga-numbers/index.html"
+  },
+  {
+    "revision": "de392d1b4cfb61010451",
+    "url": "/saga-numbers/static/css/main.048c8380.chunk.css"
+  },
+  {
+    "revision": "b0c7cfcacbcaa0ad97dc",
+    "url": "/saga-numbers/static/js/2.99cc62a6.chunk.js"
+  },
+  {
+    "revision": "de392d1b4cfb61010451",
+    "url": "/saga-numbers/static/js/main.b4aec141.chunk.js"
+  },
+  {
+    "revision": "bd8dcf8e3ee330e31d23",
+    "url": "/saga-numbers/static/js/runtime~main.22b5068e.js"
+  },
+  {
+    "revision": "3a0fcb8033b4f2d4948201f76d302278",
+    "url": "/saga-numbers/static/media/logo.3a0fcb80.svg"
+  }
+]);
